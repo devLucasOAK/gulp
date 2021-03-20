17583b6dc83f8a20a23711d3e61bc5615ac90fa4
devLucasOAK <devlucasoak@outook.com>
2021-03-20 19:00:37 -0300

    gulp 4 upgrade

--- gulpfile.js
+++ gulpfile.js
@@ -9,33 +9,47 @@ var mainBowerFiles = require('main-bower-files');
 var gulpUglify = require('gulp-uglify');
 
 
-gulp.task('libs', function(){
+
+gulp.task('libs', gulp.series(function(){
     return gulp.src(mainBowerFiles())
-    .pipe(angularFileSort())
-    .pipe(concat('libs.js'))
-    .pipe(gulpUglify())
-    .pipe(gulp.dest('dist/src'));
-});
+        .pipe(angularFileSort())
+        .pipe(concat('libs.js'))
+        .pipe(gulpUglify())
+        .pipe(gulp.dest('dist/src'));
+}));
 
 gulp.task('appModules', function (){
     return gulp.src('app/js/**/*.js')
-    .pipe(ngAnnotate())
-    .pipe(angularFileSort())
-    .pipe(concat('scripts.js'))
-    .pipe(gulp.dest('dist/src'));
+        .pipe(ngAnnotate())
+        .pipe(angularFileSort())
+        .pipe(concat('scripts.js'))
+        .pipe(gulp.dest('dist/src'));
 });
 
-gulp.task('css', function(){
+gulp.task('css', gulp.series(function(){
     return gulp.src('app/css/*.css')
-    .pipe(concat('style.css'))
-    .pipe(gulp.dest('dist/src'));
-});
+        .pipe(concat('style.css'))
+        .pipe(gulp.dest('dist/src'));
+}));
 
-gulp.task('templates', function(){
+gulp.task('templates', gulp.series(function(){
     return gulp.src('app/templates/*.html')
-    .pipe(templateCache('template.js', {
-        module: 'quizApp',
-        root: 'app/templates'
+        .pipe(templateCache('template.js', {
+            module: 'quizApp',
+            root: 'app/templates'
     }))
     .pipe(gulp.dest('dist/src'));
-});
+}));
+
+gulp.task('index', gulp.series(function(){
+    return gulp.src('app/index.html')
+        .pipe(gulp.dest('dist/'));
+}));
+
+
+gulp.task('appMain', gulp.series(function(){
+    return gulp.src('app/app.js')
+        .pipe(gulp.dest('dist/'));
+}));
+
+gulp.task('build', gulp.parallel('index', 'templates', 'css', 'libs', 'appModules', 'appMain'));
